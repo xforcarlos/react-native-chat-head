26ee3e138bac63fd4be585f914d3db505a73c162
Mahmoud hamdy <you@example.com>
2023-11-16 12:11:36 +0200

    fix(bug): warning on ios

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,43 @@
 import { NativeModules, Platform } from 'react-native';
 
+const isAndroid = Platform.OS === 'android';
+const _logWarning = () =>
+  console.warn('react-native-chat-head is not supported on iOS');
 const LINKING_ERROR =
   `The package 'react-native-chat-head' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
-const ChatHead = NativeModules.ChatHead
+const ChatHead = isAndroid
   ? NativeModules.ChatHead
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+    ? NativeModules.ChatHead
+    : new Proxy(
+        {},
+        {
+          get() {
+            throw new Error(LINKING_ERROR);
+          },
+        }
+      )
+  : null;
 
 export function showChatHead(): Promise<boolean> {
-  return ChatHead.showChatHead();
+  return isAndroid ? ChatHead.showChatHead() : _logWarning();
 }
 export function hideChatHead(): Promise<boolean> {
-  return ChatHead.hideChatHead();
+  return isAndroid ? ChatHead.hideChatHead() : _logWarning();
 }
 export function updateChatBadgeCount(count: number): Promise<boolean> {
   if (typeof count !== 'number') {
     throw new Error('count must be a number');
   }
-  return ChatHead.updateBadgeCount(count);
+  return isAndroid ? ChatHead.updateBadgeCount(count) : _logWarning();
 }
+
+const chatHead = {
+  showChatHead,
+  hideChatHead,
+  updateChatBadgeCount,
+};
+export default chatHead;
